d1b7db35f2314ba99c9b80decef7818cca2adc68
Adam Jedlička <adajedlicka@gmail.com>
2021-09-03 15:50:28 +0200

    Add analyze build option

--- packages/core/src/Build.ts
+++ packages/core/src/Build.ts
@@ -1,9 +1,13 @@
 import path from 'path'
+import open from 'open'
 import { build } from 'vite'
+import rollupPluginVisualizer from 'rollup-plugin-visualizer'
 import { Fir } from './Fir'
 
 export class Build extends Fir {
   async build(): Promise<void> {
+    const analyze = process.argv.includes('--analyze')
+
     await build(
       this.buildConfig({
         clearScreen: false,
@@ -12,9 +16,21 @@ export class Build extends Fir {
           ssrManifest: true,
           outDir: path.join(this.getDistDir(), 'client'),
           emptyOutDir: true,
+          rollupOptions: {
+            plugins: [
+              analyze &&
+                rollupPluginVisualizer({
+                  filename: path.resolve(this.getDistDir(), 'client', 'stats.html'),
+                }),
+            ],
+          },
         },
       }),
     )
+
+    if (analyze) {
+      open(path.resolve(this.getDistDir(), 'client', 'stats.html'))
+    }
 
     await build(
       this.buildConfig({
