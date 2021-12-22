967f0ac5588fcff484591bfee3fb8568fe6b6c90
Adam Jedlička <adajedlicka@gmail.com>
2021-12-22 13:28:21 +0100

    Add test for react complex app

--- packages/react/test/Basic.spec.ts
+++ packages/react/test/Basic.spec.ts
@@ -13,3 +13,36 @@ test('serves React app', async ({ page }) => {
     },
   )
 })
+
+test('serves complex React app', async ({ page }) => {
+  await makeProject(
+    {
+      packages: [
+        '@fir/base',
+        '@fir/react',
+        [
+          'my-package',
+          {
+            templates: {
+              'App.jsx': `
+                import { useState } from 'react'
+                export default function App() {
+                  const [count, setCount] = useState(0)
+
+                  return <button onClick={() => setCount(count + 1)}>Count {count}</button>
+                }
+              `,
+            },
+          },
+        ],
+      ],
+    },
+    async ({ get }) => {
+      const { text } = await get(page, '/')
+      expect(text).toContain('0')
+      await expect(page.locator('button')).toContainText('0')
+      await page.locator('button').click()
+      await expect(page.locator('button')).toContainText('1')
+    },
+  )
+})
