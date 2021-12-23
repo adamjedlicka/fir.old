5f1983de781b7a665695b7b72d0788fd3b047e07
Adam Jedlička <adajedlicka@gmail.com>
2021-12-23 11:01:17 +0100

    Test refactor

---
+++ packages/core/test/Config.spec.ts
@@ -0,0 +1,52 @@
+import { test, expect } from '@playwright/test'
+import { makeProject } from '@fir/testing/Utils'
+
+test('vite config merging', async ({ page }) => {
+  await makeProject(
+    {
+      packages: [
+        '@fir/base',
+        '@fir/vue',
+        '@fir/vue-router',
+        [
+          'my-package',
+          {
+            'pages': {
+              'index.vue': `
+                <template>{{ test }}</template>
+                <script>
+                import test from '../test.test'
+                export default {
+                  setup() {
+                    return {
+                      test
+                    }
+                  }
+                }
+                </script>
+              `,
+            },
+            'test.test': 'Hello, World!',
+            'vite.config.ts': `
+              export default {
+                plugins: [
+                  {
+                    transform(code, id) {
+                      if (id.endsWith('.test')) {
+                        return 'export default \\\'' + code + '\\\''
+                      }
+                    },
+                  },
+                ]
+              }
+            `,
+          },
+        ],
+      ],
+    },
+    async ({ url }) => {
+      await page.goto(url)
+      await expect(page.locator('#app')).toHaveText('Hello, World!')
+    },
+  )
+})
--- packages/vue/test/Core.spec.ts
+++ packages/vue/test/Core.spec.ts
@@ -1,52 +1,119 @@
 import { test, expect } from '@playwright/test'
 import { makeProject } from '@fir/testing/Utils'
 
-test('vite config merging', async ({ page }) => {
+test('serves an app', async ({ page }) => {
+  await makeProject(
+    {
+      packages: ['@fir/base', '@fir/vue'],
+    },
+    async ({ url }) => {
+      await page.goto(url)
+      await expect(page.locator('#app')).toHaveText('Hello, Fir!')
+    },
+  )
+})
+
+test('supports payload', async ({ page }) => {
   await makeProject(
     {
       packages: [
         '@fir/base',
         '@fir/vue',
-        '@fir/vue-router',
         [
-          'my-package',
+          'ma-package',
           {
-            'pages': {
-              'index.vue': `
-                <template>{{ test }}</template>
+            templates: {
+              'App.vue.ejs': `
+                <template>
+                  <div>Hello, World!</div>
+                </template>
                 <script>
-                import test from '../test.test'
+                import { usePayload } from '@fir/vue/compositions/Payload'
                 export default {
-                  setup() {
-                    return {
-                      test
-                    }
+                  setup: () => {
+                    const payload = usePayload()
+                    payload.set('some payload data')
                   }
                 }
                 </script>
               `,
             },
-            'test.test': 'Hello, World!',
-            'vite.config.ts': `
-              export default {
-                plugins: [
-                  {
-                    transform(code, id) {
-                      if (id.endsWith('.test')) {
-                        return 'export default \\\'' + code + '\\\''
-                      }
-                    },
-                  },
-                ]
-              }
-            `,
           },
         ],
       ],
     },
     async ({ url }) => {
       await page.goto(url)
-      await expect(page.locator('#app')).toHaveText('Hello, World!')
+      await expect(page.locator('body')).toContainText('some payload data')
+    },
+  )
+})
+
+test('supports custom fetchers', async ({ page }) => {
+  await makeProject(
+    {
+      packages: [
+        '@fir/base',
+        '@fir/vue',
+        [
+          'ma-package',
+          {
+            templates: {
+              'App.vue.ejs': `
+                <template>
+                  <div>some {{ data }} here</div>
+                </template>
+                <script>
+                import { createFetcher } from '@fir/vue/utils/CoreUtils'
+                const useFetcher = createFetcher({
+                  fetcher: async () => 'data'
+                })
+                export default {
+                  setup: async () => {
+                    const { data } = await useFetcher()
+                    return {
+                      data
+                    }
+                  }
+                }
+                </script>
+              `,
+            },
+          },
+        ],
+      ],
+    },
+    async ({ url }) => {
+      await page.goto(url)
+      await expect(page.locator('body')).toContainText('some data here')
+      await expect(page.locator('body')).toContainText('"data"')
+    },
+  )
+})
+
+test('supports ejs templates', async ({ page }) => {
+  await makeProject(
+    {
+      packages: [
+        '@fir/base',
+        '@fir/vue',
+        [
+          'my-package',
+          {
+            templates: {
+              'App.vue.ejs': `
+                <template>
+                  <h1><%= "A" %></h1>
+                </template>
+              `,
+            },
+          },
+        ],
+      ],
+    },
+    async ({ url }) => {
+      await page.goto(url)
+      await expect(page.locator('h1')).toHaveText('A')
     },
   )
 })
