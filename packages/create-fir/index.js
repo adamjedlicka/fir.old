4b760b142ea70acbbc4a5a28af95586f8bb59b2c
Adam Jedlička <adajedlicka@gmail.com>
2021-12-23 14:46:59 +0100

    Update create-vite

--- packages/create-fir/index.js
+++ packages/create-fir/index.js
@@ -2,5 +2,37 @@
 
 const path = require('path')
 const fs = require('fs-extra')
+const prompts = require('prompts')
 
-fs.copySync(path.join(__dirname, 'template-vue'), path.join(process.cwd(), 'fir'))
+const main = async () => {
+  const responses = await prompts(
+    {
+      type: 'text',
+      name: 'directory',
+      message: 'Target directory',
+      initial: 'fir',
+    },
+    {
+      onCancel: () => {
+        throw new Error('Cancelled')
+      },
+    },
+  )
+
+  const src = path.join(__dirname, 'template-vue')
+  const dst = path.join(process.cwd(), responses.directory)
+
+  console.log('')
+  console.log('Scaffolding project in %s', dst)
+  console.log('')
+
+  await fs.copy(src, dst)
+
+  console.log('Done. Now run:')
+  console.log('')
+  console.log(`  cd ${path.relative(process.cwd(), dst)}`)
+  console.log('  yarn install')
+  console.log('  yarn dev')
+}
+
+main().catch((e) => console.error(e.message))
