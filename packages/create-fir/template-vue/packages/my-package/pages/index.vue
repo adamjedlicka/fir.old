e46fd698b7a84288e4f425d792f5d8e905370c97
Adam Jedlička <adajedlicka@gmail.com>
2021-12-25 13:50:08 +0100

    Update default template

--- packages/create-fir/template-vue/packages/my-package/pages/index.vue
+++ packages/create-fir/template-vue/packages/my-package/pages/index.vue
@@ -1,3 +1,23 @@
 <template>
+  <img src="../assets/logo.svg" />
   <h1>Hello, Fir!</h1>
+  <button @click="increment">Click me: {{ count }}</button>
 </template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const count = ref(0)
+
+const increment = () => {
+  count.value++
+}
+</script>
+
+<style>
+#app {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+</style>
