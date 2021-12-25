f8cfdb51ba5a88e925bff21d186554e0d2ee3f93
Adam Jedlička <adajedlicka@gmail.com>
2021-12-25 13:21:13 +0100

    Update create-fir

--- packages/create-fir/index.js
+++ packages/create-fir/index.js
@@ -3,36 +3,97 @@
 const path = require('path')
 const fs = require('fs-extra')
 const prompts = require('prompts')
+const minimist = require('minimist')
+const { green, red } = require('kolorist')
+
+const argv = minimist(process.argv.slice(2))
+const cwd = process.cwd()
 
 const main = async () => {
-  const responses = await prompts(
-    {
-      type: 'text',
-      name: 'directory',
-      message: 'Target directory',
-      initial: 'fir',
-    },
-    {
-      onCancel: () => {
-        throw new Error('Cancelled')
-      },
-    },
-  )
+  const responses = await getResponses()
 
   const src = path.join(__dirname, 'template-vue')
-  const dst = path.join(process.cwd(), responses.directory)
+  const dst = path.join(cwd, responses.directory)
 
-  console.log('')
-  console.log('Scaffolding project in %s', dst)
-  console.log('')
+  console.log(`\nScaffolding project in ${dst}...`)
+
+  if (responses.overwrite) await fs.emptyDir(dst)
 
   await fs.copy(src, dst)
 
-  console.log('Done. Now run:')
+  const pkgInfo = pkgFromUserAgent(process.env.npm_config_user_agent)
+  const pkgManager = pkgInfo ? pkgInfo.name : 'npm'
+
+  console.log(`\n${green('✔')} Done. Now run:\n`)
+  if (dst != cwd) {
+    console.log(`  cd ${path.relative(cwd, dst)}`)
+  }
+  switch (pkgManager) {
+    case 'yarn':
+      console.log('  yarn')
+      console.log('  yarn dev')
+      break
+    default:
+      console.log(`  ${pkgManager} install`)
+      console.log(`  ${pkgManager} run dev`)
+      break
+  }
   console.log('')
-  console.log(`  cd ${path.relative(process.cwd(), dst)}`)
-  console.log('  yarn install')
-  console.log('  yarn dev')
 }
 
-main().catch((e) => console.error(e.message))
+async function getResponses() {
+  const targetDir = argv._[0]
+
+  try {
+    return await prompts(
+      [
+        {
+          type: targetDir ? null : 'text',
+          name: 'directory',
+          message: 'Target directory',
+          initial: 'fir-app',
+        },
+        {
+          type: (_, { directory }) => (fs.existsSync(directory) && !isEmpty(directory) ? 'confirm' : null),
+          name: 'overwrite',
+          message: (_, { directory }) =>
+            (directory === '.' ? 'Current directory' : `Target directory "${directory}"`) +
+            ` is not empty. Remove existing files and continue?`,
+        },
+        {
+          type: (_, { overwrite } = {}) => {
+            if (overwrite === false) {
+              throw new Error(`${red('✖')} Operation cancelled`)
+            }
+            return null
+          },
+          name: 'overwriteChecker',
+        },
+      ],
+      {
+        onCancel: () => {
+          throw new Error(`${red('✖')} Operation cancelled`)
+        },
+      },
+    )
+  } catch (e) {
+    console.log(e.message)
+    process.exit(0)
+  }
+}
+
+function isEmpty(path) {
+  return fs.readdirSync(path).length === 0
+}
+
+function pkgFromUserAgent(userAgent) {
+  if (!userAgent) return undefined
+  const pkgSpec = userAgent.split(' ')[0]
+  const pkgSpecArr = pkgSpec.split('/')
+  return {
+    name: pkgSpecArr[0],
+    version: pkgSpecArr[1],
+  }
+}
+
+main().catch(console.error)
