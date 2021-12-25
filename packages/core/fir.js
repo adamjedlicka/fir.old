96017328fb6b0c6b4982508d6ba8b437430ec272
Adam Jedlička <adajedlicka@gmail.com>
2021-12-25 15:28:11 +0100

    Allow custom port & host

--- packages/core/fir.js
+++ packages/core/fir.js
@@ -3,26 +3,34 @@
 require('ts-node').register({ transpileOnly: true, skipIgnore: true })
 
 const path = require('path')
+const minimist = require('minimist')
 const { Dev } = require('./src/Dev')
 const { Build } = require('./src/Build')
 const { Serve } = require('./src/Serve')
 const { default: config } = require(path.join(process.cwd(), 'fir.config'))
 
+const argv = minimist(process.argv.slice(2))
+
 const main = async () => {
-  if (process.argv[2] === 'dev') {
+  const cmd = argv._[0]
+
+  const host = argv.host ?? 'localhost'
+  const port = argv.port ?? '8080'
+
+  if (cmd === 'dev') {
     const cmd = new Dev(config)
     await cmd.bootstrap()
     const server = await cmd.createServer()
-    server.listen(8080, () => console.log('Server listening on http://localhost:8080'))
-  } else if (process.argv[2] === 'build') {
+    server.listen(port, () => console.log(`Server listening on http://${host}:${port}`))
+  } else if (cmd === 'build') {
     const cmd = new Build(config)
     await cmd.bootstrap()
     await cmd.build()
-  } else if (process.argv[2] === 'serve') {
+  } else if (cmd === 'serve') {
     const cmd = new Serve(config)
     await cmd.bootstrap()
     const server = await cmd.createServer()
-    server.listen(8080, () => console.log('Server listening on http://localhost:8080'))
+    server.listen(port, () => console.log(`Server listening on http://${host}:${port}`))
   } else {
     throw 'Unknown command'
   }
